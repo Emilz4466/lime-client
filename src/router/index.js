850375dff6d4304ea2785aecbbb8058280290922
Emil Zyberski <e.zyberski@streamsoft.pl>
2022-10-30 17:40:06 +0100

    C5 - config

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginView from "../views/LoginView.vue";
+import DashboardView from "../views/DashboardView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "login",
+    component: LoginView,
+  },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: DashboardView,
+    meta: {
+      authRequired: true,
+    },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "login",
-      component: LoginView,
-    },
-    {
-      path: "/dashboard",
-      name: "dashboard",
-      component: () => import("../views/DashboardView.vue"),
-      meta: {
-        authRequired: true,
-      }
-    },
-  ],
+  routes,
 });
 
 export default router;
